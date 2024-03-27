1009032680a1d1aa62a8a6669dbd2289fb263f11
Ali Mahmoudi <ali.mahmodi@gmail.com>
2024-03-27 21:52:21 +0330

    Add project files.

---
+++ MongoDB.Microservice.Blog/BlogEntity.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+using ThirdParty.Json.LitJson;
+
+namespace MongoDB.Microservice.Blog
+{
+    public class BlogEntity
+    {
+        [BsonId]
+        [BsonElement("_id")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        //[JsonIgnore]
+        public string? id { get; set; }
+        //public int? id { get; set; }
+        public string? Title { get; set; }
+        //public int? creatorId { get; set; }
+        //public string? creatorName { get; set; }
+        //public string? creatorImageUrl { get; set; }
+        //public DateTime? createDate { get; set; }
+        public DateTime? publishDate { get; set; }
+  
+    }
+    public class BlogDetails
+    {
+        [BsonId]
+        [BsonElement("_id")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? id { get; set; }
+       // public int? id { get; set; }
+        public string? Title { get; set; }
+        public string? body { get; set; }
+        public int? creatorId { get; set; }
+        public string? creatorName { get; set; }
+        public string? creatorImageUrl { get; set; }
+        public DateTime? createDate { get; set; }
+        public DateTime? publishDate { get; set; }
+        //public bool IsPublished { get; set; }
+        public int version { get; set; } = 0;
+        //public DateTime? LatestUpdateDate { get; set; }
+    }
+}
---
+++ MongoDB.Microservice.Blog/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDB.Microservice.Blog.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class HomeController : ControllerBase
+    {
+
+        
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Task<List<BlogEntity>> Get(int pageSize = 10, int pageIndex = 1)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var filter = Builders<BlogDetails>.Filter.Empty;
+            var blogs = client.GetDatabase("MongoBlog").GetCollection<BlogDetails>("Blogs")
+                .Find(Builders<BlogDetails>.Filter.Empty)
+                .SortByDescending(e => e.publishDate)
+                .Skip((pageIndex - 1) * pageSize)
+                .Limit(pageSize)
+                .Project(x => new BlogEntity {id = x.id, Title = x.Title, publishDate = x.publishDate })
+                .ToListAsync();
+
+            return blogs;
+        }
+
+        [HttpGet]
+        public Task<BlogDetails> Details(string  id)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var filter = Builders<BlogDetails>.Filter.Empty;
+            var blogs = client.GetDatabase("MongoBlog").GetCollection<BlogDetails>("Blogs")
+                .Find(Builders<BlogDetails>.Filter.Eq(e=>e.id ,id ))
+                .FirstOrDefaultAsync();
+
+            return blogs;
+        }
+    }
+}
