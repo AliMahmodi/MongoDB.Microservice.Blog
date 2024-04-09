4bfb30c80e1da85bdb8e84807281f1ac0db0e1fb
Ali Mahmoudi <ali.mahmodi@gmail.com>
2024-04-09 05:45:19 +0330

    Add CORS policy to Program.cs

--- MongoDB.Microservice.Blog/Program.cs
+++ MongoDB.Microservice.Blog/Program.cs
@@ -3,11 +3,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(c => c.AddPolicy("CORSpolicy", 
+    p => 
+    p.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:5173")
+));
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors("CORSpolicy");
 app.UseAuthorization();
 
 app.MapControllers();
